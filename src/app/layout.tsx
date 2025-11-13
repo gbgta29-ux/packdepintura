2e8ae9abeb0c1fd6c620a413e9e10f33eb8d96fe
Gabriel Gomes <gbgta29@gmail.com>
2025-11-13 02:06:58 +0000

    diminua o zoom que ta vindo de forma padrão quando acessa pelo celular,

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
